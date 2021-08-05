60c6d1a83f5739c088862a0710af8d960fdce9b3
zbs <zhangbaoshuai@cph.citic>
2021-08-05 11:49:32 +0800

    feat: 自定义shiro Realm

---
+++ src/main/java/com/zbs/AuthenticationCustomRalmTest.java
@@ -0,0 +1,47 @@
+package com.zbs;
+
+import com.zbs.beans.CustomRalm;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
+/**
+ * description: AuthenticationCustomRalmTest
+ * date: 2021/8/5 11:33
+ * author: zhangbs
+ * version: 1.0
+ */
+public class AuthenticationCustomRalmTest {
+
+    @Test
+    public void testAuthentication() {
+
+        // 实现自己的 Realm 实例
+        CustomRalm myRealm = new CustomRalm();
+
+        // 1.构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(myRealm);
+
+        // 2.主体提交认证请求
+        // 设置SecurityManager环境
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        // 获取当前主体
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("wmyskxz", "123456");
+        // 登录
+        subject.login(token);
+
+        // subject.isAuthenticated()方法返回一个boolean值,用于判断用户是否认证成功
+        System.out.println("isAuthenticated:" + subject.isAuthenticated()); // 输出true
+        // 判断subject是否具有admin和user两个角色权限,如没有则会报错
+        subject.checkRoles("admin", "user");
+        //  subject.checkRole("xxx"); // 报错
+        // 判断subject是否具有user:add权限
+        subject.checkPermission("user:add");
+    }
+
+}
