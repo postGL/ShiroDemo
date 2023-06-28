a6da3bee44807980935f8a43b3f89dd6abe4e6bb
zbs <250468591@qq.com>
2023-06-28 10:01:31 +0800

    refactor: 文档补充

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ShiroDemo
+
+http://www.voycn.com/article/zuiquanshirojiaochengyipianxuehuishiroquanxianguanli
+bilibili上Java涛哥视频学习的Shiro框架所整理笔记，学完思路清晰，原文代码大致相同，原视频链接附上，方便以后学习查看。
+本文项目源码加数据库
+
+
+【权限管理框架】一文看懂Shiro权限管理框架！！！
+https://blog.csdn.net/weixin_47533244/article/details/127668172
--- src/main/java/com/zbs/config/MyShiroRealm.java
+++ src/main/java/com/zbs/config/MyShiroRealm.java
@@ -26,6 +26,38 @@ public class MyShiroRealm extends AuthorizingRealm {
     @Resource
     private UserInfoService userInfoService;
 
+    /**
+     * 认证：获取认证的安全数据（从数据库查询到的用户正确数据）
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // 获取用户输入的账户
+        String username = (String) authenticationToken.getPrincipal();
+        System.out.println(authenticationToken.getPrincipal());
+        // 通过username从数据库中查找 UserInfo 对象
+        // 实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
+        UserInfo userInfo = userInfoService.findByUsername(username);
+        if (null == userInfo) {
+            return null;
+        }
+
+        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(
+                username, // 用户名
+                userInfo.getPassword(), // 密码
+                ByteSource.Util.bytes(userInfo.getSalt()), // salt=username+salt
+                getName() // realm name
+        );
+        return simpleAuthenticationInfo;
+    }
+
+    /**
+     * 授权：获取授权数据(角色权限信息)
+     * @param principalCollection
+     * @return
+     */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         // 能进入这里说明用户已经通过验证了
@@ -40,24 +72,4 @@ public class MyShiroRealm extends AuthorizingRealm {
         return simpleAuthorizationInfo;
     }
 
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        // 获取用户输入的账户
-        String username = (String) authenticationToken.getPrincipal();
-        System.out.println(authenticationToken.getPrincipal());
-        // 通过username从数据库中查找 UserInfo 对象
-        // 实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
-        UserInfo userInfo = userInfoService.findByUsername(username);
-        if (null == userInfo) {
-            return null;
-        }
-
-        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(
-                userInfo, // 用户名
-                userInfo.getPassword(), // 密码
-                ByteSource.Util.bytes(userInfo.getSalt()), // salt=username+salt
-                getName() // realm name
-        );
-        return simpleAuthenticationInfo;
-    }
 }
